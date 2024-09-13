2589eb11d723e21e3ccb82168aa7d12a146dad1e
bls-gelistirici-1 <bls.yazilim.2@hotmail.com>
2024-09-13 09:21:05 +0300

    İmza bilgileri dinamik hale getirildi.Birden fazla dosya gönderme eklendi.

--- MailGonder/Form1.Designer.cs
+++ MailGonder/Form1.Designer.cs
@@ -44,6 +44,24 @@
             txtDosya = new TextBox();
             button1 = new Button();
             label1 = new Label();
+            label2 = new Label();
+            txtUnvan = new TextBox();
+            lblAdSoyad = new Label();
+            txtAdSoyad = new TextBox();
+            label4 = new Label();
+            lblFirmaAd = new Label();
+            txtFirmaAd = new TextBox();
+            lblMailAdres = new Label();
+            txtMailAdres = new TextBox();
+            lblCepTelefon = new Label();
+            txtCepTelefon = new TextBox();
+            lblLinkedin = new Label();
+            txtLinkedin = new TextBox();
+            lblGithub = new Label();
+            txtGithub = new TextBox();
+            txtFirmaLogo = new TextBox();
+            btnLogo = new Button();
+            lblFirmaLogo = new Label();
             SuspendLayout();
             // 
             // lblGondericiMail
@@ -57,7 +75,7 @@
             // 
             // txtGondericiMail
             // 
-            txtGondericiMail.Location = new Point(14, 54);
+            txtGondericiMail.Location = new Point(12, 54);
             txtGondericiMail.Name = "txtGondericiMail";
             txtGondericiMail.Size = new Size(142, 23);
             txtGondericiMail.TabIndex = 1;
@@ -73,10 +91,10 @@
             // 
             // txtGondericiSifre
             // 
-            txtGondericiSifre.Location = new Point(14, 99);
+            txtGondericiSifre.Location = new Point(12, 99);
             txtGondericiSifre.Name = "txtGondericiSifre";
             txtGondericiSifre.Size = new Size(142, 23);
-            txtGondericiSifre.TabIndex = 1;
+            txtGondericiSifre.TabIndex = 2;
             // 
             // lblAliciMail
             // 
@@ -89,17 +107,17 @@
             // 
             // txtAliciMail
             // 
-            txtAliciMail.Location = new Point(14, 148);
+            txtAliciMail.Location = new Point(12, 148);
             txtAliciMail.Name = "txtAliciMail";
             txtAliciMail.Size = new Size(142, 23);
-            txtAliciMail.TabIndex = 1;
+            txtAliciMail.TabIndex = 3;
             // 
             // txtMesaj
             // 
             txtMesaj.Location = new Point(195, 99);
             txtMesaj.Name = "txtMesaj";
-            txtMesaj.Size = new Size(338, 72);
-            txtMesaj.TabIndex = 2;
+            txtMesaj.Size = new Size(419, 72);
+            txtMesaj.TabIndex = 7;
             txtMesaj.Text = "";
             // 
             // lblMesaj
@@ -124,15 +142,15 @@
             // 
             txtKonu.Location = new Point(195, 54);
             txtKonu.Name = "txtKonu";
-            txtKonu.Size = new Size(100, 23);
-            txtKonu.TabIndex = 1;
+            txtKonu.Size = new Size(142, 23);
+            txtKonu.TabIndex = 4;
             // 
             // btnGonder
             // 
-            btnGonder.Location = new Point(237, 183);
+            btnGonder.Location = new Point(234, 325);
             btnGonder.Name = "btnGonder";
             btnGonder.Size = new Size(75, 23);
-            btnGonder.TabIndex = 3;
+            btnGonder.TabIndex = 16;
             btnGonder.Text = "Gönder";
             btnGonder.UseVisualStyleBackColor = true;
             btnGonder.Click += btnGonder_Click;
@@ -143,20 +161,21 @@
             // 
             // btnDosya
             // 
-            btnDosya.Location = new Point(328, 53);
+            btnDosya.Location = new Point(539, 53);
             btnDosya.Name = "btnDosya";
             btnDosya.Size = new Size(75, 23);
-            btnDosya.TabIndex = 3;
+            btnDosya.TabIndex = 5;
             btnDosya.Text = "Dosya Seç";
             btnDosya.UseVisualStyleBackColor = true;
             btnDosya.Click += btnDosya_Click;
             // 
             // txtDosya
             // 
-            txtDosya.Location = new Point(406, 53);
+            txtDosya.Location = new Point(380, 54);
+            txtDosya.Multiline = true;
             txtDosya.Name = "txtDosya";
-            txtDosya.Size = new Size(127, 23);
-            txtDosya.TabIndex = 1;
+            txtDosya.Size = new Size(153, 23);
+            txtDosya.TabIndex = 6;
             // 
             // button1
             // 
@@ -172,18 +191,169 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
             label1.ForeColor = Color.Red;
-            label1.Location = new Point(158, 9);
+            label1.Location = new Point(221, 9);
             label1.Name = "label1";
             label1.Size = new Size(223, 21);
             label1.TabIndex = 4;
             label1.Text = "Mail Gönderme Uygulaması";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(10, 262);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Ünvanı";
+            // 
+            // txtUnvan
+            // 
+            txtUnvan.Location = new Point(12, 280);
+            txtUnvan.Name = "txtUnvan";
+            txtUnvan.Size = new Size(142, 23);
+            txtUnvan.TabIndex = 9;
+            // 
+            // lblAdSoyad
+            // 
+            lblAdSoyad.AutoSize = true;
+            lblAdSoyad.Location = new Point(10, 218);
+            lblAdSoyad.Name = "lblAdSoyad";
+            lblAdSoyad.Size = new Size(63, 15);
+            lblAdSoyad.TabIndex = 0;
+            lblAdSoyad.Text = "Adı Soyadı";
+            // 
+            // txtAdSoyad
+            // 
+            txtAdSoyad.Location = new Point(12, 236);
+            txtAdSoyad.Name = "txtAdSoyad";
+            txtAdSoyad.Size = new Size(142, 23);
+            txtAdSoyad.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(311, 184);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 17);
+            label4.TabIndex = 4;
+            label4.Text = "İmza Bilgileri";
+            // 
+            // lblFirmaAd
+            // 
+            lblFirmaAd.AutoSize = true;
+            lblFirmaAd.Location = new Point(10, 306);
+            lblFirmaAd.Name = "lblFirmaAd";
+            lblFirmaAd.Size = new Size(58, 15);
+            lblFirmaAd.TabIndex = 0;
+            lblFirmaAd.Text = "Firma Adı";
+            // 
+            // txtFirmaAd
+            // 
+            txtFirmaAd.Location = new Point(12, 324);
+            txtFirmaAd.Name = "txtFirmaAd";
+            txtFirmaAd.Size = new Size(142, 23);
+            txtFirmaAd.TabIndex = 10;
+            // 
+            // lblMailAdres
+            // 
+            lblMailAdres.AutoSize = true;
+            lblMailAdres.Location = new Point(193, 262);
+            lblMailAdres.Name = "lblMailAdres";
+            lblMailAdres.Size = new Size(66, 15);
+            lblMailAdres.TabIndex = 0;
+            lblMailAdres.Text = "Mail Adresi";
+            // 
+            // txtMailAdres
+            // 
+            txtMailAdres.Location = new Point(195, 280);
+            txtMailAdres.Name = "txtMailAdres";
+            txtMailAdres.Size = new Size(142, 23);
+            txtMailAdres.TabIndex = 12;
+            // 
+            // lblCepTelefon
+            // 
+            lblCepTelefon.AutoSize = true;
+            lblCepTelefon.Location = new Point(194, 218);
+            lblCepTelefon.Name = "lblCepTelefon";
+            lblCepTelefon.Size = new Size(76, 15);
+            lblCepTelefon.TabIndex = 0;
+            lblCepTelefon.Text = "Cep Telefonu";
+            // 
+            // txtCepTelefon
+            // 
+            txtCepTelefon.Location = new Point(195, 236);
+            txtCepTelefon.Name = "txtCepTelefon";
+            txtCepTelefon.Size = new Size(142, 23);
+            txtCepTelefon.TabIndex = 11;
+            // 
+            // lblLinkedin
+            // 
+            lblLinkedin.AutoSize = true;
+            lblLinkedin.Location = new Point(380, 306);
+            lblLinkedin.Name = "lblLinkedin";
+            lblLinkedin.Size = new Size(52, 15);
+            lblLinkedin.TabIndex = 0;
+            lblLinkedin.Text = "Linkedin";
+            // 
+            // txtLinkedin
+            // 
+            txtLinkedin.Location = new Point(382, 324);
+            txtLinkedin.Name = "txtLinkedin";
+            txtLinkedin.Size = new Size(153, 23);
+            txtLinkedin.TabIndex = 15;
+            // 
+            // lblGithub
+            // 
+            lblGithub.AutoSize = true;
+            lblGithub.Location = new Point(379, 262);
+            lblGithub.Name = "lblGithub";
+            lblGithub.Size = new Size(43, 15);
+            lblGithub.TabIndex = 0;
+            lblGithub.Text = "Github";
+            // 
+            // txtGithub
+            // 
+            txtGithub.Location = new Point(380, 280);
+            txtGithub.Name = "txtGithub";
+            txtGithub.Size = new Size(153, 23);
+            txtGithub.TabIndex = 14;
+            // 
+            // txtFirmaLogo
+            // 
+            txtFirmaLogo.Location = new Point(379, 236);
+            txtFirmaLogo.Name = "txtFirmaLogo";
+            txtFirmaLogo.Size = new Size(154, 23);
+            txtFirmaLogo.TabIndex = 6;
+            // 
+            // btnLogo
+            // 
+            btnLogo.Location = new Point(539, 236);
+            btnLogo.Name = "btnLogo";
+            btnLogo.Size = new Size(75, 23);
+            btnLogo.TabIndex = 5;
+            btnLogo.Text = "Logo Seç";
+            btnLogo.UseVisualStyleBackColor = true;
+            btnLogo.Click += btnLogo_Click;
+            // 
+            // lblFirmaLogo
+            // 
+            lblFirmaLogo.AutoSize = true;
+            lblFirmaLogo.Location = new Point(379, 217);
+            lblFirmaLogo.Name = "lblFirmaLogo";
+            lblFirmaLogo.Size = new Size(67, 15);
+            lblFirmaLogo.TabIndex = 0;
+            lblFirmaLogo.Text = "Firma Logo";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(555, 219);
+            ClientSize = new Size(628, 364);
+            Controls.Add(label4);
             Controls.Add(label1);
+            Controls.Add(btnLogo);
             Controls.Add(btnDosya);
             Controls.Add(button1);
             Controls.Add(btnGonder);
@@ -193,9 +363,25 @@
             Controls.Add(lblAliciMail);
             Controls.Add(txtGondericiSifre);
             Controls.Add(lblGondericiSifre);
+            Controls.Add(txtFirmaLogo);
             Controls.Add(txtDosya);
             Controls.Add(txtKonu);
             Controls.Add(lblKonu);
+            Controls.Add(txtGithub);
+            Controls.Add(txtCepTelefon);
+            Controls.Add(lblFirmaLogo);
+            Controls.Add(lblGithub);
+            Controls.Add(lblCepTelefon);
+            Controls.Add(txtAdSoyad);
+            Controls.Add(lblAdSoyad);
+            Controls.Add(txtFirmaAd);
+            Controls.Add(txtLinkedin);
+            Controls.Add(txtMailAdres);
+            Controls.Add(lblLinkedin);
+            Controls.Add(lblFirmaAd);
+            Controls.Add(lblMailAdres);
+            Controls.Add(txtUnvan);
+            Controls.Add(label2);
             Controls.Add(txtGondericiMail);
             Controls.Add(lblGondericiMail);
             Name = "Form1";
@@ -224,5 +410,23 @@
         private TextBox txtDosya;
         private Button button1;
         private Label label1;
+        private Label label2;
+        private TextBox txtUnvan;
+        private Label lblAdSoyad;
+        private TextBox txtAdSoyad;
+        private Label label4;
+        private Label lblFirmaAd;
+        private TextBox txtFirmaAd;
+        private Label lblMailAdres;
+        private TextBox txtMailAdres;
+        private Label lblCepTelefon;
+        private TextBox txtCepTelefon;
+        private Label lblLinkedin;
+        private TextBox txtLinkedin;
+        private Label lblGithub;
+        private TextBox txtGithub;
+        private TextBox txtFirmaLogo;
+        private Button btnLogo;
+        private Label lblFirmaLogo;
     }
 }
--- MailGonder/Form1.cs
+++ MailGonder/Form1.cs
@@ -28,7 +28,14 @@ namespace MailGonder
                 string aliciEmail = txtAliciMail.Text;
                 string konu = txtKonu.Text;
                 string mesaj = txtMesaj.Text;
-
+                string unvan = txtUnvan.Text;
+                string adsoyad = txtAdSoyad.Text;
+                string firmaAdi = txtFirmaAd.Text;
+                string cepTelefon = txtCepTelefon.Text;
+                string mailAdres = txtMailAdres.Text;
+                string github = txtGithub.Text;
+                string linkedin = txtLinkedin.Text;
+                string firmaLogo = txtFirmaLogo.Text;
                 SmtpClient smtpClient = new SmtpClient();
                 string smtpServer = "";
                 int smtpPort = 0;
@@ -62,34 +69,56 @@ namespace MailGonder
                 mailMessage.Body = $@"
             <h1 style='color: blue; font-family: Arial, sans-serif;'>Merhaba!</h1>
             <p style='font-size: 16px; color: gray;'>{mesaj}</p>
-            <img src='cid:foto' style='border: 1px solid #000; width: 300px; height: auto;' />
             <hr style='border: 0; border-top: 1px solid #ccc; margin: 20px 0;' />
             <div style='display: flex; align-items: center;'>
-                <img src='https://media.licdn.com/dms/image/v2/C560BAQEKJ1puP35ZKQ/company-logo_200_200/company-logo_200_200/0/1630666865388?e=2147483647&v=beta&t=rtBcbSZFAw1KNdRCCuldwZ1LSGrKF3leYG8onnXve0g' style='width: 100px; height: auto; margin-right: 20px;' />
+                <img src='cid:firmaLogo' style='width: 100px; height: auto; margin-right: 20px;' />
                 <div>
                     <p style='font-size: 14px; color: #333; margin: 0;'>
-                        <strong>Burak Yýlmam</strong><br />
-                        Bilgisayar Mühendisi<br />
-                        BLS Birleþik Lastik A.Þ<br />
-                        Telefon: (539) 439-8213<br />
+                        <strong>{adsoyad}</strong><br />
+                        {unvan}<br />
+                        {firmaAdi}<br />
+                        Telefon: {cepTelefon}<br />
                         <p style='font-size: 14px; color: #333; margin: 0;'>
                         <strong>Ýletiþim Bilgileri:</strong><br />
-                        E-Posta: <a href=""mailto:burakyilmamm@hotmail.com"" style='color: #1a73e8; text-decoration: none;'>burakyilmamm@hotmail.com</a><br />
-                        Portföy: <a href='https://github.com/burakyilmam' style='color: #1a73e8; text-decoration: none;'>GitHub Profilim</a><br />
-                        Linkedin: <a href='https://tr.linkedin.com/in/burak-y%C4%B1lmam-6a21191a7' style='color: #1a73e8; text-decoration: none;'>LinkedIn Profilim</a>
+                        E-Posta: <a href=""mailto:{mailAdres}"" style='color: #1a73e8; text-decoration: none;'>{mailAdres}</a><br />
+                        Portföy: <a href='{github}' style='color: #1a73e8; text-decoration: none;'>GitHub Profilim</a><br />
+                        Linkedin: <a href='{linkedin}' style='color: #1a73e8; text-decoration: none;'>LinkedIn Profilim</a>
                         </p>
                     </p>
                 </div>
             </div>
         ";
 
+                // Bir Dosya Gönderme
+                //if (!string.IsNullOrEmpty(txtDosya.Text))
+                //{
+                //    Attachment inline = new Attachment(txtDosya.Text);
+                //    inline.ContentId = "foto";
+                //    inline.ContentDisposition.Inline = true;
+                //    inline.ContentDisposition.DispositionType = DispositionTypeNames.Inline;
+                //    mailMessage.Attachments.Add(inline);
+                //}
+
+                // Toplu Dosya Gönderme
                 if (!string.IsNullOrEmpty(txtDosya.Text))
                 {
-                    Attachment inline = new Attachment(txtDosya.Text);
-                    inline.ContentId = "foto";
-                    inline.ContentDisposition.Inline = true;
-                    inline.ContentDisposition.DispositionType = DispositionTypeNames.Inline;
-                    mailMessage.Attachments.Add(inline);
+                    string[] dosyaYollari = txtDosya.Text.Split(new[] { ", " }, StringSplitOptions.None);
+
+                    foreach (string dosyaYolu in dosyaYollari)
+                    {
+                        Attachment attachment = new Attachment(dosyaYolu);
+                        mailMessage.Attachments.Add(attachment);
+                    }
+                }
+                // Firma Logosu Gönderme
+                if (!string.IsNullOrEmpty(firmaLogo))
+                {
+                    Attachment inlineLogo = new Attachment(firmaLogo);
+                    inlineLogo.ContentId = "firmaLogo";
+                    inlineLogo.ContentDisposition.Inline = true;
+                    inlineLogo.ContentDisposition.DispositionType = DispositionTypeNames.Inline;
+                    inlineLogo.ContentType.MediaType = "image/jpeg";
+                    mailMessage.Attachments.Add(inlineLogo);
                 }
 
                 smtpClient.Send(mailMessage);
@@ -108,24 +137,46 @@ namespace MailGonder
         private void btnDosya_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
             openFileDialog.Title = "Bir dosya seçin";
 
+            // Tek Dosya
+            //if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    string dosyaYolu = openFileDialog.FileName;
+            //    txtDosya.Text = dosyaYolu;
+            //}
+
+            //Çoklu Dosya
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string dosyaYolu = openFileDialog.FileName;
-                txtDosya.Text = dosyaYolu;
+                string[] dosyaYollari = openFileDialog.FileNames;
+                txtDosya.Text = string.Join(", ", dosyaYollari);
             }
         }
 
         private void btnGizle_Click(object sender, EventArgs e)
         {
-            if(txtGondericiSifre.UseSystemPasswordChar == true)
+            if (txtGondericiSifre.UseSystemPasswordChar == true)
             {
                 txtGondericiSifre.UseSystemPasswordChar = false;
             }
             else
             {
                 txtGondericiSifre.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void btnLogo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+            openFileDialog.Title = "Bir dosya seçin";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string dosyaYolu = openFileDialog.FileName;
+                txtFirmaLogo.Text = dosyaYolu;
             }
         }
     }
